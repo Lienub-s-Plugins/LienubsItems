ec252957be42553016d2f92c6eca7475c3950987
Lienub <lienub.68@gmail.com>
2024-06-20 20:35:52 +0200

    init commit

---
+++ src/main/java/lienub/dev/lienubsitems/LienubsItems.java
@@ -0,0 +1,17 @@
+package lienub.dev.lienubsitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LienubsItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
